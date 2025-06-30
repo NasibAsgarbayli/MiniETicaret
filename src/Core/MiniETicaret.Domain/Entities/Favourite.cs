d569e97f6e87be1a7ce21707f43db347b2491bbe
NasibAsgarbayli <asgarbaylinasib@gmail.com>
2025-07-01 00:56:27 +0400

    Entites

---
+++ src/Core/MiniETicaret.Domain/Entities/Favourite.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniETicaret.Domain.Entities;
+
+public class Favourite:BaseEntity
+{
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ src/Core/MiniETicaret.Domain/Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniETicaret.Domain.Entities;
+
+public class Order:BaseEntity
+{
+    public DateTime OrderDate { get; set; }
+    public string Status { get; set; }
+
+    public ICollection<OrderProduct> OrderProducts { get; set; }
+}
